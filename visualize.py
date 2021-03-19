a20e2dddf6378878c90025fa6a4475e374dd074b
xtinaushakova <kristina.ushakova@outlook.com>
2021-03-19 12:05:35 +0300

    Made the code a little bit nicer

--- visualize.py
+++ visualize.py
@@ -1,38 +1,42 @@
 from tkinter import *
 from grid import *
 
-root = Tk()
-var = StringVar()
-
-def update_label():
-    g = Grid()
-    coords = [x for x in itertools.product(range(30), range(30))]
-    live_cells = random.sample(coords, 20)
-    g.populate(live_cells)
-    var.set(g.display())
+## UTILS ##
+def update_label(content):
+    var.set(content)
     root.update()
 
-def create_widgets():
+def create_widgets(content):
+    global label, start, finish
     label = Label(root, textvariable=var)
-    start = Button(root, text="Start", command=update_label)
+    start = Button(root, text="Start", command=update_label(content))
     finish = Button(root, text="Finish", command=root.destroy)
 
-def pack_widgets():
+def pack_widgets(*args):
     label.pack()
     start.pack()
     finish.pack()
 
+def center_window():
+    w = root.winfo_reqwidth()
+    h = root.winfo_reqheight()
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+    root.geometry('+%d+%d' % (x, y)) ## this part allows you to only change the location
+
+## MAIN ##
+root = Tk()
+var = StringVar()
+
 root.minsize(width=400, height=400)
-w = root.winfo_reqwidth()
-h = root.winfo_reqheight()
-ws = root.winfo_screenwidth()
-hs = root.winfo_screenheight()
-x = (ws/2) - (w/2)
-y = (hs/2) - (h/2)
-root.geometry('+%d+%d' % (x, y)) ## this part allows you to only change the location
+g = Grid()
+coords = [x for x in itertools.product(range(30), range(30))]
+live_cells = random.sample(coords, 20)
+g.populate(live_cells)
 
-create_widgets()
+center_window()
+create_widgets(g.display())
 pack_widgets()
-
-
 root.mainloop()
